fb4054686b0b4afd3b68406a7ec4985491e63973
vijenderchimma <vijenderchimma424@gmail.com>
2023-06-13 09:50:19 +0000

    navbar with context

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,30 +1,67 @@
-// Write your code here
 import {Link} from 'react-router-dom'
+
 import ThemeContext from '../../context/ThemeContext'
+
 import './index.css'
 
-const Navbar = () => {
-  const toggleTheme = () => {}
+const Navbar = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme, toggleTheme} = value
 
-  return (
-    <ThemeContext.Consumer>
-      <div>
-        <Link to="/" className="nav-link">
-          <img src="" className="" alt="" />
-        </Link>
-        <div>
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
+      const onToggleTheme = () => {
+        toggleTheme()
+      }
+
+      const themeImageURL = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/light-theme-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/dark-theme-img.png'
+
+      const navbarBgClassName = isDarkTheme
+        ? 'navbar-bg-dark'
+        : 'navbar-bg-light'
+
+      const websiteLogoImageURL = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/website-logo-dark-theme-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/website-logo-light-theme-img.png'
+
+      const navItemClassName = isDarkTheme
+        ? 'list-text-dark-theme'
+        : 'list-text-light-theme'
+
+      return (
+        <div className={`navbar ${navbarBgClassName}`}>
+          <div className="navbar-content">
+            <img
+              className="website-logo"
+              src={websiteLogoImageURL}
+              alt="website logo"
+            />
+            <ul className="nav-menu">
+              <li className="nav-menu-item">
+                <Link to="/" className={`nav-link ${navItemClassName}`}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-menu-item">
+                <Link to="/about" className={`nav-link ${navItemClassName}`}>
+                  About
+                </Link>
+              </li>
+            </ul>
+            <button
+              data-testid="theme"
+              className="theme-button"
+              type="button"
+              onClick={onToggleTheme}
+            >
+              <img className="theme-image" src={themeImageURL} alt="theme" />
+            </button>
+          </div>
         </div>
-        <button type="button" className="button" onClick={toggleTheme}>
-          <img src="" className="" alt="" />
-        </button>
-      </div>
-    </ThemeContext.Consumer>
-  )
-}
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
 export default Navbar
